79447307b3c4fdb756261834a7cdc42f62283583
yagya <yagyagoel87@gmail.com>
2024-04-04 17:37:39 +0530

    added the subpipeline to get watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -316,6 +316,54 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
     .status(200)
     .json(new ApiResponse(200,channel[0],"user channel fetched successfully"))
 })
+const getWatchHistory =  asyncHandler(async(req,res)=>{
+    const user= await User.aggregate([
+        {
+            $match : {
+                _id : new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup :{
+                from : "videos",
+                localField: "watchHistory",
+                foreignField : "_id",
+                as : "watchHistory",
+                pipeline : [
+                    {
+                        $lookup : {
+                            from : "users",
+                            localField : "owner",
+                            foreignField : "_id",
+                            as : "owner",
+                            pipeline: [
+                                {
+                                    $project : {
+                                        fullName: 1,
+                                        username : 1,
+                                        avatar : 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner : {
+                                $first : "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user[0].watchHistory,"watch history fetched successfully"))
+
+
+})
 export {registerUser
     ,loginUser
     ,logoutUser
@@ -325,4 +373,5 @@ export {registerUser
 ,updateAccountDetails
 ,updateUserAvatar
 ,updateUserCoverImage,
-getUserChannelProfile}
+getUserChannelProfile,
+getWatchHistory}
