44b160d8ec91adcb05f6ad7fc8612e170d282ebb
yagya <yagyagoel87@gmail.com>
2024-04-12 18:57:12 +0530

    added the controllers of subscription model, also of tweet model and of healthcheck

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,17 +7,120 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-  // TODO: toggle subscription
+  const sub  =true
+  // toggle subscription
+  if(!channnelId)
+  throw new ApiError(400,"channel  id is required")
+  const subExist = await  Subscription.aggregate([
+    {$match :{
+      channel :channelId
+    }
+  },
+  {
+    $match : {
+      subscriber:new mongoose.Types.ObjectId(req.user._id)
+    }
+  } 
+  ])
+  if(subExist[0])
+  { 
+    sub = false
+    const subdelete  = await  Subscription.deleteOne({
+    channel : channelId,
+    subscriber : req.user._id
+  })
+  }
+  else
+  {
+    const createSub = await Subscription.create({
+      channel : channelId,
+      subscriber :req.user._id
+    })
+  }
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,{subscriptionNow :sub },"toggled the subscription"))
+  
 });
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+  if(!channelId)
+  {
+    throw new ApiError(400,"channel id is required")
+
+  }
+  const subscriptions = Subscription.aggregate([
+    {
+      $match : {
+        channel : channelId
+      }
+    },
+    {
+      $lookup :{
+        from : "users",
+        localField : "channel",
+        foreignField : "_id",
+        as : "ownerDetails"
+      }
+    },
+    {
+      $lookup :{
+        from : "users",
+        localField: "subscriber",
+        foreignField : "_id",
+        as : "subscribers"
+      },
+      $project :{
+        username: 1,
+        fullName : 1,
+        avatar : 1
+      }
+    },
+    {
+      $addFields :{
+        owner : ownerDetails[0]
+      }
+    },
+  ])
+  return res
+  .status(200)
+  .json(new ApiResponse(200,subscriptions,"subs fetched successfully"))
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+  if(!subscriberId)
+  throw new ApiError(400,"subscriber id is required")
+  const channelList  =  Subscription.aggregate([
+    {
+      $match : {
+        subscriber : subscriberId
+      },
+      
+
+    },
+    {
+      $lookup : {
+        from : "users",
+        localField : "channel",
+        foreignField : "_id",
+        as : "subscribedTo"
+      },
+      $project : {
+        fullName : 1,
+        username: 1,
+        avatar: 1
+      }
+    },
+  ])
+  return res 
+  .status(200)
+  .json(200,channelList,"channell list fetched successfully")
 });
+
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,19 +6,58 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-  //TODO: create tweet
+  // create tweet
+  const {content} = req.body;
+  if(!content)
+  throw new ApiError(400,"content is required")
+  const tweet = await Tweet.create({
+    owner :req.user._id,
+    content :content
+  })
+  if(!tweet)
+  throw new ApiError(500,"there was some issue while creating the tweet")
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
-  // TODO: get user tweets
+  //  get user tweets
+  const {userId} =req.body;
+  if(!userId)
+  throw new ApiError(400,"user id is required") 
+  const tweets = await Tweet.findById(userId).select("-_id -owner")
+  return res
+  .status(200)
+  .json(new ApiResponse(200,tweets,"tweets fetched successfully"))
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
-  //TODO: update tweet
+  // update tweet
+  const {content,tweetId} =req.body;
+  if(!content||!tweetId)
+  throw new ApiError(400,"content and tweet id are required")
+  const updatedContent = await Tweet.updateById({
+  _id : tweetId},{
+    $set: {content}
+  })
+  if(!updateTweet)
+  throw new ApiError(500,"there was some issue while updating tthe tweet")
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
+  const {tweetId} = req.body;
+  if(!tweetId)
+  throw new ApiError(400,"tweet id is required")
+  const deleted = await Tweet.deleteOne({
+    _id : tweetId
+  })
+  if(!deleted.n)
+  {
+    throw new ApiError(400,"there was no tweet associated to that id")
+
+  }
+  return res
+  .status(200)
+  .json(200,{},"tweet deleted successfully")
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
