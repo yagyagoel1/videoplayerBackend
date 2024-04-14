b57ca7856df443e3dbe1d38dd1014fd25aba63ff
yagya <yagyagoel87@gmail.com>
2024-04-14 20:00:42 +0530

    added the get video comments add update and delete comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,21 +5,62 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-  //TODO: get all comments for a video
+  // get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+  if(!videoId)
+  throw new ApiError(400,"video id is requried ")
+  const skiplimit = (page-1)*limit
+  const comments = await Comment.find({video:videoId}).skip(skiplimit).limit(limit).select("-_id")
+  return res
+  .status(200)
+  .json(new ApiResponse(200,comments,`comments fetched successfully from ${skiplimit} to ${limit*page}`))
 });
 
+
 const addComment = asyncHandler(async (req, res) => {
-  // TODO: add a comment to a video
+  //  add a comment to a video
+  const {content,videoId} =req.body;
+  if(!content&&!videoId)
+  {
+    throw new ApiError(400,"video id and content are required")
+  }
+  const createdComment = await Comment.create({
+    content,
+    video : videoId,
+    owner : req.user._id
+  })
+  const getComment  = await Comment.findById(createdComment._id)
+  return res
+  .status(200)
+  .json(new ApiResponse(200,getComment,"comment added successfully"))
 });
 
 const updateComment = asyncHandler(async (req, res) => {
-  // TODO: update a comment
+  // update a comment
+  const {commentId,newContent} = req.body;
+  if(!commentId&&!newContent)
+  throw new ApiError(400,"comment id and content are required")
+  const updatedComment  = await Comment.updateOne({_id: commentId},{
+    $set : {
+      content :newContent
+    }
+  })
+  return res
+  .status(200)
+  .json(new ApiResponse(200,updateComment,"comment updated successfully"))
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
-  // TODO: delete a comment
+  // delete a comment
+  const {commentId} = req.body;
+  if(!commentId)
+  throw new ApiError(400,"comment id is required")
+  await Comment.deleteOne({_id : commentId})
+   return res
+   .status(200)
+   .json(new ApiResponse(200,{},"comment deleted successfully ")) 
+  
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
