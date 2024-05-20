2ae31cfed3f77b3909fa0bec048ecf6c71e883e7
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-21 00:18:51 +0530

    added various rotue

--- src/app.js
+++ src/app.js
@@ -17,9 +17,39 @@ app.use(cookieParser());
 
 //routes
 import userRouter from "./routes/user.route.js";
+import videoRouter from "./routes/video.route.js"
+import commentRouter from "./routes/comment.route.js"
+import tweetRouter from "./routes/tweet.route.js"
+import playlistRouter from "./routes/playlist.route.js"
+import healthcheckRouter from "./routes/healthcheck.route.js";
+import subscriptionRouter from "./routes/subscription.route.js"
+import likeRouter from "./routes/like.route.js"
+import dashboardRouter from "./routes/dashboard.route.js"
+import { ApiResponse } from "./utils/ApiResponse.js";
+
 
 //routes declaration
-
+app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/dashboard", dashboardRouter)
+
+
+// error handler
+app.use((err, req, res, next) => {
+  console.log("stack error ::", err.stack);
+    return res.status(err.statusCode || 500).json(
+        new ApiResponse(
+            err.statusCode || 500,
+            null,
+            err.message || "Error From Server"
+        )
+    );
+});
 
 export { app };
